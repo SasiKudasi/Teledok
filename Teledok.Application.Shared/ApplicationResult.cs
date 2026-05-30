fad6576bbfa76537128da40008722505a1e1c824
SasaiKudasai <sasha.mittal@yandex.ru>
2026-05-30 18:12:31 +0300

    Add Application result

---
+++ Teledok.Application.Shared/ApplicationResult.cs
@@ -0,0 +1,20 @@
+﻿namespace Teledok.Application.Shared
+{
+    public class ApplicationResult <T> 
+    {
+        public bool IsSuccess { get; }
+        public T Value { get; }
+        public string? ErrorMessage { get; }
+
+        private ApplicationResult(bool isSuccess, T value, string? errorMsg)
+        {
+            IsSuccess = isSuccess;
+            Value = value;
+            ErrorMessage = errorMsg;
+        }
+
+        public static ApplicationResult<T> Success(T value) => new(true, value, null);
+        public static ApplicationResult<T> Error(string errorMsg) => new(false, default, errorMsg);
+
+    }
+}
--- Teledok.Application/Models/PersonDto.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Teledok.Application.Models;
-
-public record PersonDto(string Inn, string Name, DateTime CreatedAt, DateTime UpdatedAt);
